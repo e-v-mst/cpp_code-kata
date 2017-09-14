c898d7e124245c3077bfb8c7870b9c40adae8a77
Mastinovskaya Elena <e.mastinovskaya@sinvent.ru>
2017-09-14 15:24:12 +0300

    add in/out from file for sort prog

--- sort/sort/main.cpp
+++ sort/sort/main.cpp
@@ -10,35 +10,49 @@ void insertionSort(int *arrayPtr, int length) // сортировка встав
     for (int counter = 1; counter < length; counter++)
     {
         temp = arrayPtr[counter]; // инициализируем временную переменную текущим значением элемента массива
-        item = counter-1; // запоминаем индекс предыдущего элемента массива
-        while(item >= 0 && arrayPtr[item] > temp) // пока индекс не равен 0 и предыдущий элемент массива больше текущего
+        item = counter-1;
+        cout << item << "  ";
+        while(item >= 0 && arrayPtr[item] > temp)
         {
-            arrayPtr[item + 1] = arrayPtr[item]; // перестановка элементов массива
+            arrayPtr[item + 1] = arrayPtr[item];
             arrayPtr[item] = temp;
-            item--;
+            --item;
         }
     }
 }
 
 int main()
 {
+    fstream infile ("input.txt");
+    if (!infile)
+    {
+        cout << "can't read file input.txt" << endl;
+        return EXIT_FAILURE;
+    }
 
-       int size_array = 10;
+    int size_array = 0;
+    infile  >> size_array;
 
+    int sorted_array[size_array];
 
-       int sorted_array[10] = {1, 8, 4, 2, 3, 7, 5, 6, 9, 0};
+    int a = 0, i = 0;
+    while (infile >> a) {
+         sorted_array[i] = a;
+         ++i;
+    }
 
-       insertionSort(sorted_array, size_array);
+    insertionSort(sorted_array, size_array);
 
-       for (int counter = 0; counter < size_array; counter++)
-       {
-           cout << sorted_array[counter] << "  ";
-       }
-       cout << "\n";
-       delete [] sorted_array;
-       system("pause");
+    ofstream outfile ("output.txt");
+    for (int counter = 0; counter < size_array; counter++)
+    {
+        //cout << sorted_array[counter] << "  ";
+        outfile << sorted_array[counter] << "  ";
+    }
 
-       return 0;
+    cout << endl;
+
+    return EXIT_SUCCESS;
 }
 
 
